aae1f230ec78841c65edd66b75bc803c2eba2e8b
DELL <gor_nick987@mail.ru>
2018-02-09 20:17:22 +0300

    init 20 lab

---
+++ 21/models/user.js
@@ -0,0 +1,16 @@
+module.exports = (Sequelize, sequelize) => {
+    return sequelize.define('users', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        email: Sequelize.STRING,
+        password: Sequelize.STRING,
+
+        firstname: Sequelize.STRING,
+        lastname: Sequelize.STRING
+    });
+};
+
